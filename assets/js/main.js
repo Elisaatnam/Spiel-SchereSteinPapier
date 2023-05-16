b0e387bc396925552f2042ea7b1cf0813663eb20
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-05-16 12:11:32 +0200

    optimized JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,213 +1,146 @@
-//globale scopes
-const rundenOutput = document.querySelector(".count-rounds article");
-const actionOutput = document.querySelector(".action-output h3");
+// +++++++++++++globale Scopes++++++++++++++++++
+// Runden
+const rundenAnzeige = document.querySelector(".runden-anzeige");
+const rundenAnzahl = document.querySelector(".runden-anzahl");
+const gespielteRunden = document.querySelector(".gespielte-runden");
+const gesamteRunden = document.querySelector(".gesamte-runden");
+const resetBtn = document.querySelector(".reset");
 
-//outputs fuer spielstaende
+//Spielstaende
 const spielstandSpieler = document.querySelector(".spielstand-spieler");
 const spielstandComputer = document.querySelector(".spielstand-computer");
 
-//Variablen fuer Punkte
+//Anzeige - gewonnen, verloren, 'lass uns spielen'
+const actionOutput = document.querySelector(".action-output");
+
+// Button Area mit Buttons: Schere, Stein, Papier
+const btnArea = document.querySelector(".choices");
+const schereBtn = document.querySelector(".schere");
+const steinBtn = document.querySelector(".stein");
+const papierBtn = document.querySelector(".papier");
+
+const makeYourMove = document.querySelector(".mach-deinen-zug");
+
+//variable festlegen um Runden zu zaehlen
+let aktuelleRunde = 0;
+
+//variablen festlegen um Spielstaende zu zaehlen
 let spielerPunkte = 0;
 let computerPunkte = 0;
 
-//Button value auslesen
-let spielerChoice = null;
-const getChoiceValue = choice => {
-	spielerChoice = choice;
-};
+//variable festlegen um spaeter zu speichern was der Computer ausgewaehlt hat
+let computerChoice = "";
 
-//Buttons fuer color change
-const btnSchere = document.querySelector(".btn1");
-const btnStein = document.querySelector(".btn2");
-const btnPapier = document.querySelector(".btn3");
+// +++++++++++++++++++++++ Funktionen +++++++++++++++++++++++
+//random Computer Choice generieren
+const randomPcChoice = () => {
+	let randomNum = Number(Math.round(Math.random() * 2 + 1));
 
-//Variable fuer aktuelle Rundenzahl
-let aktuelleRunde = 0;
-//rundenanzahl auslesen
-let runde = null;
-const getRoundValue = num => {
-	runde = num;
-};
-
-//++++++++++++  play funktion +++++++++++++
-const play = () => {
-	event.preventDefault();
-
-	//random computer choice generieren
-	let computerChoice = Math.floor(Math.random() * 3) + 1;
-	if (computerChoice === 1) {
+	if (randomNum === 1) {
 		computerChoice = "Schere";
-	} else if (computerChoice === 2) {
+	} else if (randomNum === 2) {
 		computerChoice = "Stein";
 	} else {
 		computerChoice = "Papier";
 	}
+};
 
-	console.log({ spielerChoice }, { computerChoice });
+// Play/Main Funktion
+const playGame = userChoice => {
+	event.preventDefault();
+	randomPcChoice();
 
-	if (aktuelleRunde < runde - 1) {
-		btnSchere.style.border = "2px solid black";
-		btnStein.style.border = "2px solid black";
-		btnPapier.style.border = "2px solid black";
-		if (spielerChoice === computerChoice) {
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = "Unentschieden!";
-		} else if (spielerChoice === "Schere" && computerChoice === "Stein") {
-			btnSchere.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${computerChoice} schlaegt ${spielerChoice}. Du verlierst 😐`;
-		} else if (spielerChoice === "Schere" && computerChoice === "Papier") {
-			btnSchere.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${spielerChoice} schlaegt ${computerChoice}. Du gewinnst 😌`;
-		} else if (spielerChoice === "Stein" && computerChoice === "Schere") {
-			btnStein.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${spielerChoice} schlaegt ${computerChoice}. Du gewinnst 😌`;
-		} else if (spielerChoice === "Stein" && computerChoice === "Papier") {
-			btnStein.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${computerChoice} schlaegt ${spielerChoice}. Du verlierst 😐`;
-		} else if (spielerChoice === "Papier" && computerChoice === "Schere") {
-			btnPapier.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${computerChoice} schlaegt ${spielerChoice}. Du verlierst 😐`;
-		} else if (spielerChoice === "Papier" && computerChoice === "Stein") {
-			btnPapier.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			actionOutput.innerHTML = `${spielerChoice} schlaegt ${computerChoice}. Du gewinnst 😌`;
+	//Rundenazahl auslesen
+	let gesamteRundenZahl = document.querySelector(
+		'input[name="num-of-rounds"]:checked',
+	).value;
+	//console.log(gesamteRundenZahl);
+
+	//Runden im HTML anzeigen
+	gesamteRunden.innerHTML = gesamteRundenZahl;
+	rundenAnzahl.classList.add("display-none");
+	rundenAnzeige.classList.add("display-block");
+
+	//Runden hochzaehlen
+	aktuelleRunde++;
+	gespielteRunden.innerHTML = aktuelleRunde;
+
+	//eigentliches Spiel, wer gewinnt/verliert + Punkte verteilen
+	if (
+		(userChoice == "Schere" && computerChoice == "Papier") ||
+		(userChoice == "Stein" && computerChoice == "Schere") ||
+		(userChoice == "Papier" && computerChoice == "Stein")
+	) {
+		spielerPunkte++;
+		actionOutput.innerHTML = `You won`;
+		if (userChoice === "Schere") {
+			schereBtn.style.animation = "green 1s linear";
+		} else if (userChoice === "Stein") {
+			steinBtn.style.animation = "green 1s linear";
+		} else if (userChoice === "Papier") {
+			papierBtn.style.animation = "green 1s linear";
 		}
-	} else if (aktuelleRunde === runde - 1) {
-		btnSchere.style.border = "2px solid black";
-		btnStein.style.border = "2px solid black";
-		btnPapier.style.border = "2px solid black";
-		if (spielerChoice === computerChoice) {
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Schere" && computerChoice === "Stein") {
-			btnSchere.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Schere" && computerChoice === "Papier") {
-			btnSchere.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Stein" && computerChoice === "Schere") {
-			btnStein.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Stein" && computerChoice === "Papier") {
-			btnStein.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Papier" && computerChoice === "Schere") {
-			btnPapier.style.animation = "red 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			computerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
-		} else if (spielerChoice === "Papier" && computerChoice === "Stein") {
-			btnPapier.style.animation = "green 1s linear 1";
-			aktuelleRunde++;
-			rundenOutput.innerHTML = `${aktuelleRunde} / ${runde}`;
-			spielerPunkte++;
-			spielstandSpieler.innerHTML = spielerPunkte;
-			spielstandComputer.innerHTML = computerPunkte;
-			if (spielerPunkte > computerPunkte) {
-				actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
-			} else if (spielerPunkte < computerPunkte) {
-				actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
-			} else {
-				actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
-			}
+	} else if (userChoice == computerChoice) {
+		actionOutput.innerHTML = `Unetschieden. Beide haben ${userChoice} gewaehlt.`;
+		if (userChoice === "Schere") {
+			schereBtn.style.animation = "orange 1s linear";
+		} else if (userChoice === "Stein") {
+			steinBtn.style.animation = "orange 1s linear";
+		} else if (userChoice === "Papier") {
+			papierBtn.style.animation = "orange 1s linear";
+		}
+	} else {
+		computerPunkte++;
+		actionOutput.innerHTML = `You loose`;
+		if (userChoice === "Schere") {
+			schereBtn.style.animation = "red 1s linear";
+		} else if (userChoice === "Stein") {
+			steinBtn.style.animation = "red 1s linear";
+		} else if (userChoice === "Papier") {
+			papierBtn.style.animation = "red 1s linear";
 		}
 	}
+
+	//Spielstand ins HTML schreiben
+	spielstandSpieler.innerHTML = spielerPunkte;
+	spielstandComputer.innerHTML = computerPunkte;
+
+	// Spiel beenden bei erreichen der ausgewaehlten Rundenanzahl
+	if (aktuelleRunde - 1 === gesamteRundenZahl - 1) {
+		if (spielerPunkte === computerPunkte) {
+			actionOutput.innerHTML = `Gleichstand! Du & der Computer haben jeweils ${spielerPunkte} Punkte.`;
+		} else if (spielerPunkte > computerPunkte) {
+			actionOutput.innerHTML = `Glueckwunsch, du hast den Computer geschlagen`;
+		} else {
+			actionOutput.innerHTML = `Du hast leider gegen den Computer verloren`;
+		}
+		gameOver();
+	}
 };
+
+//  Game Over Funktion
+const gameOver = () => {
+	gesamteRundenZahl = 0;
+	btnArea.classList.add("display-none");
+	makeYourMove.classList.add("display-none");
+};
+
+schereBtn.addEventListener("click", () => playGame("Schere"));
+steinBtn.addEventListener("click", () => playGame("Stein"));
+papierBtn.addEventListener("click", () => playGame("Papier"));
+
+//reset function
+resetBtn.addEventListener("click", () => {
+	spielerPunkte = 0;
+	computerPunkte = 0;
+	aktuelleRunde = 0;
+	actionOutput.innerHTML = "Lass uns spielen";
+
+	spielstandSpieler.innerHTML = 0;
+	spielstandComputer.innerHTML = 0;
+
+	btnArea.classList.remove("display-none");
+	makeYourMove.classList.remove("disply-none");
+	rundenAnzahl.classList.remove("display-none");
+	rundenAnzeige.classList.remove("display-block");
+});
